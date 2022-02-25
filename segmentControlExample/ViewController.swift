11b3b40c9d7ffd969aae5375bcb48754b93eb170
Erhan <demirci.erhan@windowslive.com>
2022-02-25 11:32:19 +0300

    Initial Commit

---
+++ segmentControlExample/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  segmentControlExample
+//
+//  Created by erhan demirci on 25.02.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
